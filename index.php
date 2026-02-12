c5de65dc117660787f61f965af1f360e5ebd1139
guendfo0109 <fguendouz61@gmail.com>
2026-02-12 09:27:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-// Faille SQL Injection
-$id = $_GET['id'];
-$query = "SELECT * FROM users WHERE id = " . $id;
-
-// Faille XSS
-$nom = $_GET['nom'];
-echo "<h1>Bienvenue " . $nom . "</h1>";
-echo "<p>Requête générée : " . $query . "</p>";
+  $name = $_GET['name'];
+  echo "<h1>Bonjour " . $name . "</h1>"; 
 ?>
